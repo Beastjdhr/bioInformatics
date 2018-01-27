7d96d059ab21345332afe513fb445c1b7dcc2704
Beastjdhr <josephdzr@gmail.com>
2018-01-27 15:54:45 -0600

    correccion

--- getList.pl
+++ getList.pl
@@ -2,39 +2,8 @@ use warnings;
 use strict;
 use feature qw(say);
 
-my $tree= 'livi.tre';
-my @orgs;
 
-open TRE, "$tree" or die $!;
-my @lines= <TRE>;
-close TRE;
-
-my $orgsLine= $lines[2];
-my @orgLines= split/\[/, $orgsLine;
-
-foreach my $oL (@orgLines) {
-  $oL=~ /'(.*)'/;
-  if ($1) {
-    my $n= $1;
-    $n=~ s/[\(\)']//g;
-      push @orgs, $n;
-  }
-  $oL=~ /,(.*)/;
-  if ($1) {
-    my $m= $1;
-    $m=~ s/[\(\)']//g;
-      push @orgs, $m;
-  }
-}
-
-open FILE, ">>orgs.txt" or die $!;
-my @F= <FILE>;
-foreach my $i (@orgs) {
-  print FILE "$i\n";
-}
-close FILE;
-
-my $bash= `cut -f 1 orgs.txt | uniq`;
+my $bash= `cut -f 1 fO.txt | uniq`;
 my @uniq= split/\n/, $bash;
 
 open TL, ">>tList.txt" or die $!;
